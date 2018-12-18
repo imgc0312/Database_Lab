99d15397394ec6e0749036118548e46712c789c7
imgc0312 <43901494+imgc0312@users.noreply.github.com>
2018-12-19 00:31:45 +0800

    Update studentPersonalDataUpdata.php
    fix string bug

--- studentPersonalDataUpdata.php
+++ studentPersonalDataUpdata.php
@@ -1,5 +1,6 @@
 <?php //學生資料 個人資料 (可供修改) 
 require("connMysql.php");
+require("ourLib.php");
 global $DB_CONNECT;
 session_start();
 $newBirthday=$_POST['Birthday'];
@@ -7,15 +8,15 @@ $newEmail=$_POST['Email'];
 
 if(isset($newBirthday)){
 	$sql = 'UPDATA student
-	SET Birthday = ' . $newBirthday . '
-	WHERE id = ' . $_SESSION['user']['id'];
+	SET Birthday = ' . ourToS($newBirthday) . '
+	WHERE id = ' . ourToS($_SESSION['user']['id']);
 	mysqli_query($DB_CONNECT, $sql);
 }
 
 if(isset($newEmail)){
 	$sql = 'UPDATA student
-	SET Email = ' . $newEmail . '
-	WHERE id = ' . $_SESSION['user']['id'];
+	SET Email = ' . ourToS($newEmail) . '
+	WHERE id = ' . ourToS($_SESSION['user']['id']);
 	mysqli_query($DB_CONNECT, $sql);
 }
 

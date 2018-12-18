51540c20149d7e37e626f8f5f6ac392ae138f071
imgc0312 <43901494+imgc0312@users.noreply.github.com>
2018-12-19 00:04:32 +0800

    upload studentPersonalDataUpdata

---
+++ studentPersonalDataUpdata.php
@@ -0,0 +1,23 @@
+<?php //學生資料 個人資料 (可供修改) 
+require("connMysql.php");
+global $DB_CONNECT;
+session.start();
+$newBirthday=$_POST['Birthday'];
+$newEmail=$_POST['Email'];
+
+if(isset($newBirthday)){
+	$sql = 'UPDATA student
+	SET Birthday = ' . $newBirthday . '
+	WHERE id = ' . $_SESSION['user']['id'];
+	mysqli_query($DB_CONNECT, $sql);
+}
+
+if(isset($newEmail)){
+	$sql = 'UPDATA student
+	SET Email = ' . $newEmail . '
+	WHERE id = ' . $_SESSION['user']['id'];
+	mysqli_query($DB_CONNECT, $sql);
+}
+
+header("location:studentPersonalData.php");
+?>

484fdc66682fbf7197ddf0ef4787a248c44065d0
Wayne <djz04jo3@gmail.com>
2018-12-20 01:20:27 +0800

    move to libs

--- connMysql.php
+++
@@ -1,7 +0,0 @@
-<?php
-	require "config/config.php";
-	
-	$DB_CONNECT = mysqli_connect(DB_LOCATION, DB_ID, DB_PASS, DB_NAME)
-					or die("MySQL connection error");
-	mysqli_query($DB_CONNECT, "SET NAMES 'utf8'");  // 設定字元集與編碼為utf8
-?>
